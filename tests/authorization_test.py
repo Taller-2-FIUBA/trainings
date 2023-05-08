8e7ab9e34f86424c5dc4a7365ea1815d51675d9c
MarianoCinalli <cinallimariano1992@gmail.com>
2023-05-07 22:23:00 -0300

    chore: get credentials from external auth service

--- tests/authorization_test.py
+++ tests/authorization_test.py
@@ -1,8 +1,56 @@
 # pylint: disable= missing-module-docstring, missing-function-docstring
+from unittest.mock import MagicMock
 from fastapi import HTTPException
 from pytest import raises
 
-from trainings.authorization import assert_can_create_training
+from trainings.authorization import (
+    assert_can_create_training, get_permissions
+)
+
+
+def test_when_external_auth_service_returns_credentials_expect_them():
+    # Setup data and expectations
+    expected_host = "auth-service.fiufit.svc.cluster.local:8002"
+    expected_url = f"http://{expected_host}/auth/credentials"
+    expected_credentials = {"role": "admin"}
+    response = MagicMock()
+    response.status_code = 200
+    response.json = MagicMock(return_value={"data": expected_credentials})
+    # Setup testing doubles
+    dummy_headers = MagicMock()
+    config_stub = MagicMock()
+    config_stub.auth.host = expected_host
+    client_spy = MagicMock()
+    client_spy.get = MagicMock(return_value=response)
+    # Exercise
+    permissions = get_permissions(dummy_headers, client_spy, config_stub)
+    # Assert data
+    assert permissions == expected_credentials
+    # Assert expectations
+    client_spy.get.assert_called_once_with(
+        expected_url, headers=dummy_headers
+    )
+
+
+def test_when_external_auth_service_returns_code_403_expect_error():
+    response = MagicMock()
+    response.status_code = 403
+    client_spy = MagicMock()
+    client_spy.get = MagicMock(return_value=response)
+    # Exercise
+    with raises(HTTPException):
+        get_permissions(MagicMock(), client_spy, MagicMock())
+
+
+def test_when_response_raises_expect_error():
+    response = MagicMock()
+    response.status_code = 200
+    response.json = MagicMock(side_effect=Exception("Boom!"))
+    client_spy = MagicMock()
+    client_spy.get = MagicMock(return_value=response)
+    # Exercise
+    with raises(HTTPException):
+        get_permissions(MagicMock(), client_spy, MagicMock())
 
 
 def test_when_admin_creates_training_expect_error():
--- trainings/authorization.py
+++ trainings/authorization.py
@@ -1,11 +1,43 @@
 """Authorize users to perform actions."""
 import logging
 from typing import Dict
-from fastapi import HTTPException, status
+
+from fastapi import HTTPException, Header, status
+from httpx import Client
+
+from trainings.config import AppConfig
+
+
+def get_permissions(
+    headers: Header,
+    http_client: Client,
+    config: AppConfig
+) -> Dict[str, str]:
+    """Get user details from token in request header."""
+    url = f"http://{config.auth.host}/auth/credentials"
+    credentials = http_client.get(url, headers=headers)
+    if credentials.status_code != status.HTTP_200_OK:
+        logging.error(
+            "Error getting token. Status code: %d, Error: %s",
+            credentials.status_code,
+            credentials.json()
+        )
+        raise HTTPException(
+            status_code=credentials.status_code,
+            detail=credentials.json()
+        )
+    try:
+        return credentials.json()["data"]
+    except Exception as json_exception:
+        msg = "Error getting credentials (Maybe token is invalid?)"
+        logging.error("%s. Credentials %s", msg, credentials)
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail=msg
+        ) from json_exception
 
 
 def assert_can_create_training(permissions: Dict[str, str]) -> bool:
-    """Raise HTTPException if user can't create training"""
+    """Raise HTTPException if user can't create training."""
     # When role accept trainer for value change this.
     logging.info("Validating that user can create trainings...")
     if permissions["role"] != "user":
