630464d23db3e50e831fa48158f2b6f6bc7fa83c
MarianoCinalli <cinallimariano1992@gmail.com>
2023-05-21 19:59:27 -0300

    chore: build certificate data from config

--- tests/firebase_test.py
+++ tests/firebase_test.py
@@ -2,7 +2,7 @@
 from unittest.mock import MagicMock, patch
 
 from hamcrest import assert_that, matches_regexp
-from trainings.firebase import get_file_name, save
+from trainings.firebase import get_certificate, get_file_name, save
 
 
 @patch("trainings.firebase.initialize_app")
@@ -45,3 +45,32 @@ def test_that_file_name_is_random():
     assert_that(first, matches_regexp("some_trainer_id([A-z0-9]{32})"))
     assert first != get_file_name("some_trainer_id")
     assert first != get_file_name("some_trainer_id")
+
+
+def test_when_getting_certificate_data_is_from_config():
+    certificate_data = {
+        "firebase.type": "type",
+        "firebase.project_id": "project_id",
+        "firebase.private_key_id": "private_key_id",
+        "firebase.private_key": "private_key",
+        "firebase.client_email": "client_email",
+        "firebase.client_id": "client_id",
+        "firebase.auth_uri": "auth_uri",
+        "firebase.token_uri": "token_uri",
+        "firebase.auth_provider_x509_cert_url": "auth_provider_x509_cert_url",
+        "firebase.client_x509_cert_url": "client_x509_cert_url",
+    }
+    config = MagicMock(**certificate_data)
+    certificate = get_certificate(config)
+    assert certificate == {
+        "type": "type",
+        "project_id": "project_id",
+        "private_key_id": "private_key_id",
+        "private_key": "private_key",
+        "client_email": "client_email",
+        "client_id": "client_id",
+        "auth_uri": "auth_uri",
+        "token_uri": "token_uri",
+        "auth_provider_x509_cert_url": "auth_provider_x509_cert_url",
+        "client_x509_cert_url": "client_x509_cert_url",
+    }
--- trainings/firebase.py
+++ trainings/firebase.py
@@ -2,6 +2,7 @@
 import logging
 import random
 import string
+from typing import Any, Dict
 from firebase_admin import initialize_app, storage
 from firebase_admin.credentials import Certificate
 
@@ -15,12 +16,29 @@ def get_file_name(trainer_id: str) -> str:
     )
 
 
+def get_certificate(config: AppConfig) -> Dict[str, Any]:
+    """Create certificate data for firebase API."""
+    return {
+        "type": config.firebase.type,
+        "project_id": config.firebase.project_id,
+        "private_key_id": config.firebase.private_key_id,
+        "private_key": config.firebase.private_key,
+        "client_email": config.firebase.client_email,
+        "client_id": config.firebase.client_id,
+        "auth_uri": config.firebase.auth_uri,
+        "token_uri": config.firebase.token_uri,
+        "auth_provider_x509_cert_url":
+        config.firebase.auth_provider_x509_cert_url,
+        "client_x509_cert_url": config.firebase.client_x509_cert_url,
+    }
+
+
 def save(media: str, trainer_id: str, config: AppConfig) -> str:
     """Save media in firebase and return id."""
     logging.debug("Saving in firebase: %s", media)
     logging.info("Initializing firebase...")
     initialize_app(
-        Certificate(config.firebase.__dict__),
+        Certificate(get_certificate(config)),
         {"storageBucket": config.firebase.storagebucket}
     )
     bucket = storage.bucket()
