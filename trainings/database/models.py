305b5c091d091d0dcdcfdd0ac348bc88dab52ab9
MarianoCinalli <cinallimariano1992@gmail.com>
2023-05-21 21:15:22 -0300

    chore: update user model

--- trainings/database/models.py
+++ trainings/database/models.py
@@ -82,7 +82,7 @@ class Users(Base):
     """Table structure for user."""
 
     __tablename__ = "users"
-    id = Column(String, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     email = Column(String)
     username = Column(String)
     name = Column(String)
