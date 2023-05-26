fedc1544a6ac2e72ee03e87907f5c0448f48c46d
MarianoCinalli <cinallimariano1992@gmail.com>
2023-05-26 19:11:27 -0300

    chore: handle unknown user input when creating training

--- trainings/database/hydrator.py
+++ trainings/database/hydrator.py
@@ -1,5 +1,7 @@
 """Hydrate database model objects from DTOs."""
 import logging
+from fastapi import HTTPException, status
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm import Session
 
 from trainings.database.models import (
@@ -20,25 +22,50 @@ def hydrate(session: Session, training: TrainingIn) -> Training:
         logging.debug(
             "Building exercise %s", training_exercise.dict()
         )
-        exercises.append(
-            TrainingExercise(
-                exercise=session.query(Exercise).filter(
-                    Exercise.name == training_exercise.name,
-                    Exercise.unit == training_exercise.unit
-                ).one(),
-                count=training_exercise.count,
-                series=training_exercise.series
+        try:
+            exercises.append(
+                TrainingExercise(
+                    exercise=session.query(Exercise).filter(
+                        Exercise.name == training_exercise.name,
+                        Exercise.unit == training_exercise.unit
+                    ).one(),
+                    count=training_exercise.count,
+                    series=training_exercise.series
+                )
             )
-        )
+        except NoResultFound as error:
+            detail = "Could not save training. Exercise "\
+                f"{training_exercise.name} {training_exercise.unit} not found."
+            logging.warning(detail)
+            raise HTTPException(
+                detail=detail, status_code=status.HTTP_404_NOT_FOUND
+            ) from error
     logging.info("Building training...")
+    try:
+        training_type = session.query(TrainingType).filter(
+            TrainingType.name == training.type
+        ).one()
+    except NoResultFound as error:
+        detail = f"Could not save training. Type {training.type} not found."
+        logging.warning(detail)
+        raise HTTPException(
+            detail=detail, status_code=status.HTTP_404_NOT_FOUND
+        ) from error
+    try:
+        training_difficulty = session.query(Difficulty).filter(
+            Difficulty.name == training.difficulty
+        ).one()
+    except NoResultFound as error:
+        detail = "Could not save training. Difficulty "\
+            f"{training.difficulty} not found."
+        logging.warning(detail)
+        raise HTTPException(
+            detail=detail, status_code=status.HTTP_404_NOT_FOUND
+        ) from error
     foreign_keys = {
         "exercises": exercises,
-        "type": session.query(TrainingType).filter(
-            TrainingType.name == training.type
-        ).one(),
-        "difficulty": session.query(Difficulty).filter(
-            Difficulty.name == training.difficulty
-        ).one(),
+        "type": training_type,
+        "difficulty": training_difficulty,
     }
     fields = training.dict() | foreign_keys
     logging.debug("Exercise built %s", fields)
